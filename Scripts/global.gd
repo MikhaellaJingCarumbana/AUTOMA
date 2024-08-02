bbaddf924d1de6af9d6fe3442109dfc84da00549
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-08-03 04:56:04 +0800

    Separate startbuttons

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -27,28 +27,47 @@ func _ready():
 	})
 	
 var active_line: Line2D = null
+var active_line2: Line2D = null
 var start_button: Button = null
+var start_button2: Button = null
 var end_button: Button = null
 
 func set_active_line(line: Line2D):
 	active_line = line
+
+func set_active_line2(line: Line2D):
+	active_line2 = line
 
 func clear_active_line():
 	active_line = null
 	start_button = null
 	end_button = null
 
+func clear_active_line2():
+	active_line2 = null
+	start_button2 = null
+	end_button = null
+
 func get_active_line() -> Line2D:
 	return active_line
 
+func get_active_line2() -> Line2D:
+	return active_line2
+
 func set_start_button(button: Button):
 	start_button = button
+
+func set_start_button2(button: Button):
+	start_button2 = button
 
 func set_end_button(button: Button):
 	end_button = button
 
 func get_start_button() -> Button:
 	return start_button
+
+func get_start_button2() -> Button:
+	return start_button2
 
 func get_end_button() -> Button:
 	return end_button
