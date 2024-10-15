5cc5012300ec064820c1f5953b1d438f69c90126
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-10-16 04:25:38 +0800

    door fixed final

--- Chapters/Chapter 1/Scripts/level.gd
+++ Chapters/Chapter 1/Scripts/level.gd
@@ -11,4 +11,4 @@ func _ready():
 func _on_level_spawn(destination_tag: String):
 	var door_path = "Door/" + destination_tag
 	var door = get_node(door_path) as Door
-	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn.global_direction)
+	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
