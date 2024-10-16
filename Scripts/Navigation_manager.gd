5dffba69088afafad5da86ecfb9fd20762ce0bf7
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-10-17 04:39:56 +0800

    Scene Transition update

--- Scripts/Navigation_manager.gd
+++ Scripts/Navigation_manager.gd
@@ -3,6 +3,7 @@ extends Node
 
 const scene_beginner = preload("res://Chapters/Chapter 1/Regex - Map/Beginner.tscn")
 const scene_intermediate = preload("res://Chapters/Chapter 1/Regex - Map/Intermediate.tscn")
+const scene_advance = preload("res://Chapters/Chapter 1/Regex - Map/Advance.tscn")
 
 
 signal on_triggr_player_spawn
@@ -17,10 +18,12 @@ func go_to_level(level_tag, destination_tag):
 			scene_to_load = scene_beginner
 		"Intermediate":
 			scene_to_load =scene_intermediate
+		"Advance":
+			scene_to_load = scene_advance
+			
 			
 	if scene_to_load != null:
 		spawn_door_tag = destination_tag
 		get_tree().change_scene_to_packed(scene_to_load)
-		
 func trigger_player_spawn(position: Vector2, direction: String):
 	on_triggr_player_spawn.emit(position, direction)
