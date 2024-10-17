6dc677d59225782e6c8ed79cc7f377e4c3b6c1db
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-10-17 15:28:33 +0800

    transition screen fixed

--- Scripts/Navigation_manager.gd
+++ Scripts/Navigation_manager.gd
@@ -21,8 +21,9 @@ func go_to_level(level_tag, destination_tag):
 		"Advance":
 			scene_to_load = scene_advance
 			
-			
 	if scene_to_load != null:
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		spawn_door_tag = destination_tag
 		get_tree().change_scene_to_packed(scene_to_load)
 func trigger_player_spawn(position: Vector2, direction: String):
