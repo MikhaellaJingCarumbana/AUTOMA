d1712760800b00e2f728db6e48fed6aeebeaa811
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2024-12-01 14:56:13 +0800

    Chapter Menu draft

--- Chapters/Main UI/Scripts/button.gd
+++ Chapters/Main UI/Scripts/button.gd
@@ -28,7 +28,7 @@ func _on_mouse_entered() -> void:
 	if tween_hover and tween_hover.is_running():
 		tween_hover.kill()
 	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-	tween_hover.tween_property(self, "scale", Vector2(1.2, 1.2), 0.5)
+	tween_hover.tween_property(self, "scale", Vector2(1.1, 1.1), 0.5)
 
 # Called when the mouse exits the button area
 func _on_mouse_exited() -> void:
@@ -40,7 +40,7 @@ func _on_mouse_exited() -> void:
 	if tween_hover and tween_hover.is_running():
 		tween_hover.kill()
 	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-	tween_hover.tween_property(self, "scale", Vector2.ONE, 0.65)
+	tween_hover.tween_property(self, "scale", Vector2.ONE, 0.60)
 
 
 
