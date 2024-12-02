9c5b4b0e1a33421d7a1dcf95bcdab5c005a3f685
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-12-03 04:56:30 +0800

    clues system draft

--- Chapters/Chapter 1/Sprite/Props animated/chests/scripts/chest.gd
+++ Chapters/Chapter 1/Sprite/Props animated/chests/scripts/chest.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @onready var sprite_2d = $AnimatedSprite2D  # Reference to the chest's AnimatedSprite2D
+@onready var clues: Area2D = $"../Clues"
 var is_chest_open = false  # Track if the chest is open or closed
 
 func _ready():
 	sprite_2d.play("Idle")  # Start with the chest closed
+	if is_instance_valid(clues):
+		clues.hide()
 
 func open_chest():
 	sprite_2d.stop()  # Stop any current animation
@@ -14,11 +17,18 @@ func open_chest():
 		if sprite_2d.sprite_frames.has_animation("Close"):
 			sprite_2d.play("Close")  # Play the closing animation
 		is_chest_open = false  # Update the state to closed
+		
+		if is_instance_valid(clues):
+			clues.hide()
+
 	else:
 		# If the chest is closed, play the open animation
 		if sprite_2d.sprite_frames.has_animation("Open"):
 			sprite_2d.play("Open")  # Play the opening animation
 		is_chest_open = true  # Update the state to open
+		
+		if is_instance_valid(clues):
+			clues.show()
 
 
 func _on_death_zone_body_entered(body: Node2D) -> void:
---
+++ Chapters/Chapter 1/Sprite/Props animated/items/clues.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	animated_sprite_2d.play("default")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body.is_in_group("Player")):
+		queue_free()
