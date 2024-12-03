c9084cf5f02f50d418822392c0f38896e57c508a
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2024-12-03 22:38:14 +0800

    Revert "draft clue collection script"
    This reverts commit a97247423680c7ef6421dcbd4a03b1237b592e9c.

--- Chapters/Chapter 1/Sprite/Props animated/items/clues.gd
+++ Chapters/Chapter 1/Sprite/Props animated/items/clues.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-@export var clue_id: String
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -16,8 +15,3 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("Player")):
 		queue_free()
-		
-func trigger_event(clue_id: String) -> void:
-	var game_manager = get_tree().root.get_node("GameManager")
-	if game_manager:
-		game_manager.clue_collected(clue_id)
