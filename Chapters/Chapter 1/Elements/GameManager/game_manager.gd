a97247423680c7ef6421dcbd4a03b1237b592e9c
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2024-12-03 21:44:55 +0800

    draft clue collection script

--- Chapters/Chapter 1/Elements/GameManager/game_manager.gd
+++ Chapters/Chapter 1/Elements/GameManager/game_manager.gd
@@ -21,4 +21,20 @@ func add_point():
 	points += 1
 	print(points)
 	
+func clue_collected(clue_id: String) -> void:
+	print("Clue collected: " + clue_id)
 	
+	
+	match clue_id:
+		"clue_1":
+			collected1()
+		"clue_2":
+			collected2()
+		_:
+			print("No Specific action for this clue")
+
+func collected1() -> void:
+	print("Collected clue 1: Trigger specific event!")
+
+func collected2() -> void:
+	print("Collected clue 2: Trigger another specific event!")
--- Chapters/Chapter 1/Sprite/Props animated/items/clues.gd
+++ Chapters/Chapter 1/Sprite/Props animated/items/clues.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var clue_id: String
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +16,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("Player")):
 		queue_free()
+		
+func trigger_event(clue_id: String) -> void:
+	var game_manager = get_tree().root.get_node("GameManager")
+	if game_manager:
+		game_manager.clue_collected(clue_id)
