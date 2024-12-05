9398daa6baa64acd7d881d804970aed5e9d53596
Tara Ysbel <taraysbel.datan@cit.edu>
2024-12-06 01:25:56 +0800

    Chapter 2 Cards Setup

---
+++ Chapters/Chapter 2/chap2_card_data.gd
@@ -0,0 +1,8 @@
+class_name Chap2CardUIData extends CardUIData
+
+
+@export var type : String
+@export var description : String
+
+func format_description():
+    return description
--- Chapters/Chapter 2/chapter_2.gd
+++ Chapters/Chapter 2/chapter_2.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var card_pile_ui := $CardPileUI
+@onready var panel_container = $PanelContainer
+@onready var rich_text_label = $PanelContainer/RichTextLabel
+
+var current_hovered_card : CardUI
+
 func _ready() -> void:
-    pass # Replace with function body.
+    card_pile_ui.draw(5)
+    card_pile_ui.connect("card_hovered", func(card_ui):
+        rich_text_label.text = card_ui.card_data.format_description()
+        panel_container.visible = true
+        current_hovered_card = card_ui
+    )
+    card_pile_ui.connect("card_unhovered", func(_card_ui):
+        panel_container.visible = false
+        current_hovered_card = null
+    )
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-    pass
+func _process(delta):
+    if current_hovered_card:
+        var target_pos = current_hovered_card.position
+        target_pos.y += 80
+        target_pos.x += 180
+        panel_container.position = target_pos
