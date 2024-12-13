62280cc3ea9126b15c31b9904e9222c994c5b3eb
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-12-14 05:54:07 +0800

    float update

--- Chapters/Chapter 1/Sprite/Props animated/items/clues2.gd
+++ Chapters/Chapter 1/Sprite/Props animated/items/clues2.gd
@@ -5,14 +5,23 @@ extends Area2D
 @onready var chest: Node = get_node(parent_chest)
 @onready var game_manager: Node = %GameManager
 # Called when the node enters the scene tree for the first time.
+
+@export var float_speed: float = 5.0
+@export var float_amplitude: float = 3.0
+
+var base_y_position: float = 0.0
+var time_elapsed: float = 0.0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	base_y_position = global_position.y
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	time_elapsed += delta
+	position.y = base_y_position + sin(time_elapsed * float_speed) * float_amplitude
 	animated_sprite_2d.play("default")
-
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and chest and chest.has_method("is_open") and chest.call("is_open"):
