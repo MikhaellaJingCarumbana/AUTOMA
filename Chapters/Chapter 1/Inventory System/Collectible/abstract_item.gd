9ae59e9644b0340a644e3ef2a2a109b052d7d12e
Mikhaella Jing Carumbana <emomj2009@gmail.com>
2024-12-26 05:45:17 +0800

    Inventory system update

--- Chapters/Chapter 1/Inventory System/Collectible/abstract_item.gd
+++ Chapters/Chapter 1/Inventory System/Collectible/abstract_item.gd
@@ -1,11 +1,12 @@
-extends AnimatedSprite2D
+extends Sprite2D
 
 
-# Called when the node enters the scene tree for the first time.
+@export var ID = "0"
+
 func _ready() -> void:
-	pass # Replace with function body.
+	texture = load("res://Art/Database png/" + ItemData.get_texture(ID))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(area: Area2D) -> void:
+	get_parent().find_child("Inventory").add_item(ID)
+	queue_free()
---
+++ Chapters/Chapter 1/Inventory System/ui.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Inventory"):
+		visible = !visible
