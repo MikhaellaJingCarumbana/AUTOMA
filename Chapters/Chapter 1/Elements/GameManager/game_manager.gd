329edaf4821d4684d4f6e78ef7e486eadc86c0a0
Jing Beifong <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-01 02:09:16 +0800

    Revert "Major Update: Sprite change and lighting"
    This reverts commit 5941fdc2da8b53acaec8723db0cf62681bb5dc4b.

--- Chapters/Chapter 1/Elements/GameManager/game_manager.gd
+++ Chapters/Chapter 1/Elements/GameManager/game_manager.gd
@@ -4,7 +4,6 @@ extends Node
 @onready var note_system: Control = $"../UI/Note System/CarouselSelection"
 @onready var clue_system: Control = $"../UI/Clue_System/CarouselSelection"
 @onready var hurt_player_effect: CanvasLayer = $"../HurtPlayerEffect"
-@onready var anim: AnimatedSprite2D = $"../Player/AnimatedSprite2D"
 
 
 
@@ -21,7 +20,6 @@ signal note_collected(index: int)
 func decrease_health():
 	lives -= 1
 	hurt_player_effect.show_hurt_effect()
-	anim.play("Hurt")
 	print(lives)
 	for h in 5:
 		if (h < lives):
