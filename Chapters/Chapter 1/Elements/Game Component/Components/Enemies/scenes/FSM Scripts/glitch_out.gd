cfdc949b8588b78672a304dd5f23945f41506931
Jing Beifong <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-12 18:23:20 +0800

    fixing dash

--- Chapters/Chapter 1/Elements/Game Component/Components/Enemies/scenes/FSM Scripts/glitch_out.gd
+++ Chapters/Chapter 1/Elements/Game Component/Components/Enemies/scenes/FSM Scripts/glitch_out.gd
@@ -1,6 +1,11 @@
 extends State
 
 @onready var map_bounds: CollisionShape2D = $Map_bounds/CollisionShape2D
+var dash_speed = 700
+var dash_distance = 200
+var is_dashing = false
+@export var dash_offset: Vector2 = Vector2(-50, 0)
+
 var can_transition1: bool = false
 # Called when the node enters the scene tree for the first time.
 func enter():
@@ -9,16 +14,14 @@ func enter():
 	await dash()
 	can_transition1 = true
 
-
 func dash():
 	var tween = create_tween()
-
-	tween.tween_property(owner, "position", Vector2(player.position.x, player.position.y), 0.5)
-	await tween.finished
 	
+	var target_position = owner.position + (direction.normalized() * dash_distance)
+	tween.tween_property(owner, "position", target_position, 0.1)
+	await tween.finished
 	
 func transition():
 	if can_transition1:
 		can_transition1 = false
-		
 		get_parent().change_state("Run")
--- Chapters/Chapter 1/Elements/Game Component/Components/Enemies/scenes/FSM Scripts/glitch_slice.gd
+++ Chapters/Chapter 1/Elements/Game Component/Components/Enemies/scenes/FSM Scripts/glitch_slice.gd
@@ -1,6 +1,10 @@
 extends State
 
 var can_tranistion1: bool = false
+var dash_speed = 700
+var dash_distance = 200
+var is_dashing = false
+@export var dash_offset: Vector2 = Vector2(-50, 0)
 @onready var map_bounds: CollisionShape2D = $Map_bounds/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
@@ -8,14 +12,13 @@ func enter():
 	super.enter()
 	animation_player.play("glitch out")
 	await dash()
+	await get_tree().create_timer(0.5).timeout
 	can_tranistion1 = true
 
 func dash():
 	var tween = create_tween()
-
-	tween.tween_property(owner, "position", Vector2(player.position.x, player.position.y), 0.5)
-	await tween.finished
-
+	
+	
 func transition():
 	if can_tranistion1:
 		can_tranistion1 = false
