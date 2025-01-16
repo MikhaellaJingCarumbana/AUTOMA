c9efdadcb0f0ea1b53c3c31929259ec821e95e57
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-16 21:36:32 +0800

    tentative chapters

--- Chapters/Main UI/chapters_new_new.gd
+++ Chapters/Main UI/chapters_new_new.gd
@@ -14,3 +14,28 @@ func _process(delta: float) -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	pass
+
+
+func _on_chapter_1_mouse_entered() -> void:
+	animation_player.play("chapter 1")
+	
+
+func _on_chapter_2_mouse_entered() -> void:
+	animation_player.play("chapter 2")
+	
+
+func _on_chapter_3_mouse_entered() -> void:
+	animation_player.play("chapter 3")
+	
+
+func _on_chapter_1_mouse_exited() -> void:
+	animation_player.play("chapter 1_2")
+	
+
+
+func _on_chapter_2_mouse_exited() -> void:
+	animation_player.play("chapter 2_2")
+	
+
+func _on_chapter_3_mouse_exited() -> void:
+	animation_player.play("chapter 3_2")
