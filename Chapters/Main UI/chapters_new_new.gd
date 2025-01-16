e97f60203bc63a38548f7d60f8afbd330165e14c
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-16 22:53:14 +0800

    draft animations

--- Chapters/Main UI/chapters_new_new.gd
+++ Chapters/Main UI/chapters_new_new.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
+var tween_hover: Tween
+
+var displacement: float = 0.0 
+var oscillator_velocity: float = 0.0
+
+var tween_rot: Tween
+var tween_destroy: Tween
+var tween_handle: Tween
+
+var last_mouse_pos: Vector2
+var mouse_velocity: Vector2
+var following_mouse: bool = false
+var last_pos: Vector2
+var velocity: Vector2
+@export var hover_scale: Vector2 = Vector2(1.2, 1.2)  # How much the button scales on hover
+@export var hover_duration: float = 0.5  # Duration of the hover effect
+
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
@@ -13,8 +31,36 @@ func _process(delta: float) -> void:
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	pass
+	if anim_name == "Intro card draw":
+		animation_player.play("chapter 1 flip")
+	elif anim_name == "chapter 1 flip":
+		animation_player.play("chapter 2 flip")
+	elif anim_name == "chapter 2 flip": 
+		animation_player.play("chapter 3 flip")
+	else:
+		print("animation complete")
+		
+
 
 
 
 	
+
+
+func _on_chapter_1_mouse_entered() -> void:
+	if tween_hover and tween_hover.is_running():
+		tween_hover.kill()
+	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween_hover.tween_property(self, "scale", Vector2(1.1, 1.1), 0.5)
+	
+
+func _on_chapter_1_mouse_exited() -> void:
+	if tween_rot and tween_rot.is_running():
+		tween_rot.kill()
+	tween_rot = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK).set_parallel(true)
+	
+	# Reset scale
+	if tween_hover and tween_hover.is_running():
+		tween_hover.kill()
+	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween_hover.tween_property(self, "scale", Vector2.ONE, 0.60)
