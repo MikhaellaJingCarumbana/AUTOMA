4f37572f3e135a415866d8434c144881915bd6ec
Jing Beifong <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-16 13:28:11 +0800

    Animation intro to boss cutscene

--- Chapters/Chapter 1/Cutscene/intro_cutscene.gd
+++ Chapters/Chapter 1/Cutscene/intro_cutscene.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.play("1")
+	animation_player.play("Intro card draw")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +15,16 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("skip"):
 		get_tree().change_scene_to_file("res://Chapters/Chapter 1/Regex - Map/Beginner-F.tscn")
+
+
+func _on_button_mouse_entered() -> void:
+	$AnimationPlayer.play("Card powerup")
+
+
+func _on_button_mouse_exited() -> void:
+	$AnimationPlayer.play("Card powerup_2")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Intro card draw":
+		animation_player.play("Take card")
