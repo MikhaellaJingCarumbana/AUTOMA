c0298acaccd4f0c891b4e4c253b0bb9575f3df29
Jing Beifong <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-18 04:31:27 +0800

    connect chapters menu

--- Chapters/Chapter 1/Cutscene/chapter_1_menu.gd
+++ Chapters/Chapter 1/Cutscene/chapter_1_menu.gd
@@ -1,22 +1,16 @@
-extends Node2D
+extends Control
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var scene: PackedScene
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.play("intro")
-	
+	animation_player.play("transition")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("skip"):
-		get_tree().change_scene_to_packed(scene)
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "intro":
+	if event.is_action_pressed("skip"):
 		get_tree().change_scene_to_packed(scene)
