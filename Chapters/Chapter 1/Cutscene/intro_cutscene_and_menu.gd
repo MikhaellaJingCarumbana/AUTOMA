bc5576fc6a074598afcaf9ddd21ce90620e1369c
Jing Beifong <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-17 12:08:47 +0800

    tentative intro changes

--- Chapters/Chapter 1/Cutscene/intro_cutscene_and_menu.gd
+++ Chapters/Chapter 1/Cutscene/intro_cutscene_and_menu.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("intro")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
