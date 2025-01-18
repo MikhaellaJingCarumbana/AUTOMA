a5c1fd9dd5c80f16f7f9f8f5122a5b1613fbdbec
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-18 12:27:17 +0800

    update

---
+++ Chapters/Chapter 1/Cutscene/cutscene_for_chapter_1.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("intro")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "intro":
+		get_tree().change_scene_to_file("res://Chapters/Chapter 1/Regex - Map/Beginner-F.tscn")
---
+++ Chapters/Chapter 1/Regex - Map/beginner_f.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("transition")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "transition":
+		$CanvasLayer2/ColorRect.visible = false
+		$CanvasLayer2.visible = false
