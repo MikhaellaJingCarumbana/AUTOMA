bb0fb54889c6a4df64c3f293fbc9e0d8a6d8c988
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-01-20 14:28:45 +0800

    Boss area update

--- Chapters/Chapter 1/Elements/Game Component/Components/Enemies/scenes/FSM Scripts/death.gd
+++ Chapters/Chapter 1/Elements/Game Component/Components/Enemies/scenes/FSM Scripts/death.gd
@@ -1,9 +1,11 @@
 extends State
 
+@onready var boss_music: AudioStreamPlayer2D = $"../../Boss Music"
 
 # Called when the node enters the scene tree for the first time.
 func enter():
 	super.enter()
 	animation_player.play("dead")
 	await animation_player.animation_finished
+	boss_music.stop()
 	#play the death screen
