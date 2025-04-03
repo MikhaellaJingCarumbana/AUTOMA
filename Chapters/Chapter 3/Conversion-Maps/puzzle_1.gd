62ed37dab26a82b6fa7bde719cc0444d8c48d96c
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2025-04-03 22:38:59 +0800

    chapter 3 update

--- Chapters/Chapter 3/Conversion-Maps/puzzle_1.gd
+++ Chapters/Chapter 3/Conversion-Maps/puzzle_1.gd
@@ -3,12 +3,16 @@ extends Control
 @onready var one: LineEdit = $ColorRect/LineEdit2
 @onready var two: LineEdit = $ColorRect/LineEdit3
 @onready var puzzle_1: Control = $"."
+@onready var key1: TextureRect = $"../Keys/HBoxContainer/TextureRect"
+
 
 func _process(delta: float) -> void:
 	# Check if player presses the submit answer action
 	if Input.is_action_just_pressed("submit answer"):
 		if one.text == "e" and two.text == "e":
 			print("Correct!")  # Replace with desired action
+			key1.visible = true
+			puzzle_1.visible = false
 		else:
 			print("Incorrect!")  # Feedback for wrong answer
 			puzzle_1.visible = false
--- Chapters/Chapter 3/Conversion-Maps/puzzle_3.gd
+++ Chapters/Chapter 3/Conversion-Maps/puzzle_3.gd
@@ -1,15 +1,15 @@
 extends Control
 
-@onready var three: LineEdit = $ColorRect/LineEdit3
+@onready var four: LineEdit = $ColorRect/LineEdit3
 @onready var puzzle_1: Control = $"."
-@onready var key2: TextureRect = $"../Keys/HBoxContainer/TextureRect2"
+@onready var key3: AnimatedSprite2D = $"../Keys/HBoxContainer/TextureRect3/AnimatedSprite2D"
 
 func _process(delta: float) -> void:
 	# Check if player presses the submit answer action
 	if Input.is_action_just_pressed("submit answer"):
-		if three.text == "aa":
+		if four.text == "aa (a+b)*" or four.text == "aa(a+b)*":
 			print("Correct!")  # Replace with desired action
-			key2.visible = true
+			key3.visible = true
 			puzzle_1.visible = false
 		else:
 			print("Incorrect!")  # Feedback for wrong answer
